05ad06375b0d9747773d8d4da807c1a9c864de85
GingerNinjaStickdudeWorld <109624855+GingerNinjaStickdudeWorld@users.noreply.github.com>
2022-09-17 14:27:34 +0100

    Add files via upload

---
+++ timeline-editor-react-master/timeline-editor-react-master/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'Timeline',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    externals: {
+        react: 'react',
+        'react-dom': 'react-dom'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000
+    }
+};
